0f6a25ff99e981038a18e7796dba62dbf9f2f3ab
Eric <98676586+Eircc@users.noreply.github.com>
2022-12-24 10:07:36 -0500

    Update CCC '00 S1 - Slot Machines.cpp

--- 2000/Senior/CCC '00 S1 - Slot Machines.cpp
+++ 2000/Senior/CCC '00 S1 - Slot Machines.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <string>
 
 using namespace std;
 
 int main() {
     int q, a, b, c;
     cin >> q >> a >> b >> c;
-    int plays = 0;
+    int ans = 0;
     while (q > 0){
-        q--;
-        plays++;
         a++;
-        if (a == 35) {
+        q--;
+        ans++;
+        if (a == 35){
             q += 30;
             a = 0;
         }
-        if (q == 0) break;
 
-        q--;
-        plays++;
-        b++;
-        if (b == 100) {
-            q += 60;
-            b = 0;
+        if (q > 0){
+            b++;
+            q--;
+            ans++;
+            if (b == 100){
+                q += 60;
+                b = 0;
+            }
         }
-        if (q == 0) break;
-
-        q--;
-        plays++;
-        c++;
-        if (c == 10) {
-            q += 9;
-            c = 0;
+        
+        if (q > 0){
+            c++;
+            q--;
+            ans++;
+            if (c == 10){
+                q += 9;
+                c = 0;
+            }
         }
-        if (q == 0) break;
     }
-    cout << "Martha plays " << plays << " times before going broke.";
+    cout << "Martha plays " << ans << " times before going broke." << endl;
 }
